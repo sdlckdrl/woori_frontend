a761e5f3717c1da5bfa2fe95b41e84fe3fad4c67
sdlckdrl <sdlckdrl@gmail.com>
2020-09-09 11:23:19 +0900

    first commit

---
+++ src/views/shop/Index.vue
@@ -0,0 +1,28 @@
+<script>
+  // Extensions
+  import View from '@/views/View'
+
+  // Mixins
+  import LoadSections from '@/mixins/load-sections'
+
+  export default {
+    name: 'Shop',
+
+    metaInfo: { title: 'Shop' },
+
+    extends: View,
+
+    mixins: [
+      LoadSections([
+        'hero-alt',
+      ]),
+    ],
+
+    props: {
+      id: {
+        type: String,
+        default: 'shop',
+      },
+    },
+  }
+</script>
---
+++ vue.config.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+  "outputDir": "D:\\project\\woori\\backend\\public",
+  "devServer": {
+    "proxy": {
+      "/api": {
+        "target": "http://localhost:3000/api",
+        "changeOrigin": true,
+        "pathRewrite": {
+          "^/api": ""
+        }
+      }
+    }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
+
+// //개발용
+// module.exports = {
+//   devServer: {
+//     proxy: { // proxyTable 설정 
+//       '/api': {
+//         target: 'http://localhost:3000/api',
+//         changeOrigin: true, 
+//         pathRewrite: { "^/api": '' }
+//       }
+//     }
+//   }
+// }
